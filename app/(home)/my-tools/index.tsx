d863641a9e91c4085be164e3f6450d6687072a9e
Original Timi <timileyinoyelekan11@gmail.com>
2024-07-30 10:44:24 +0100

    design: my-tools page

--- app/(home)/my-tools/index.tsx
+++ app/(home)/my-tools/index.tsx
@@ -1,78 +1,105 @@
 import { createStyleSheet, UnistylesRuntime, useStyles } from 'react-native-unistyles';
-import { Text, View } from '~/components/shared';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import Entypo from '@expo/vector-icons/Entypo';
-import { Image, ScrollView, TouchableOpacity } from 'react-native';
-import { useState } from 'react';
+import { TextInput, View } from '~/components/shared';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { ScrollView, TouchableOpacity } from 'react-native';
 import GoBack from '~/components/go-back';
-
+import Tool, { TOOL_STATUS } from '~/components/Tool';
+import { useState } from 'react';
 const topInset = UnistylesRuntime.insets.top;
 const bottomInset = UnistylesRuntime.insets.bottom;
 
+const data = [
+  {
+    title: 'Cordless Impact Driver',
+    category: 'Machine',
+    description:
+      'This impact driver offers high torque and variable speed, making it perfect for heavy-duty tasks.',
+    status: 'available',
+    lastUsed: new Date('2024-07-29T12:34:56Z'),
+    id: '2',
+  },
+  {
+    title: 'Laser Level',
+    category: 'Measurement',
+    description:
+      'Provides accurate level and alignment measurements with bright laser lines for visibility.',
+    status: 'unavailable',
+    lastUsed: new Date('2024-07-28T09:15:30Z'),
+    id: '3',
+  },
+  {
+    title: 'Angle Grinder',
+    category: 'Machine',
+    description:
+      'A versatile tool for grinding, cutting, and polishing with a powerful motor and ergonomic design.',
+    status: 'available',
+    lastUsed: new Date('2024-07-25T17:22:14Z'),
+    id: '4',
+  },
+  {
+    title: 'Digital Caliper',
+    category: 'Measurement',
+    description:
+      'High-precision digital caliper for measuring internal and external dimensions with an easy-to-read display.',
+    status: 'available',
+    lastUsed: new Date('2024-07-30T08:45:00Z'),
+    id: '5',
+  },
+  {
+    title: 'Heat Gun',
+    category: 'Machine',
+    description:
+      'Multi-functional heat gun with adjustable temperature settings for stripping paint, thawing pipes, and more.',
+    status: 'unavailable',
+    lastUsed: new Date('2024-07-20T14:10:50Z'),
+    id: '6',
+  },
+  {
+    title: 'Oscillating Tool',
+    category: 'Machine',
+    description: 'An essential tool for cutting, sanding, scraping, and grinding in tight spaces.',
+    status: 'available',
+    lastUsed: new Date('2024-07-22T10:55:33Z'),
+    id: '7',
+  },
+];
 
 const MyToolsScreen = () => {
   const { styles } = useStyles(_styles);
-  const [showOption, setShowOption] = useState(false);
-
-  const handleToggle = () => {
-    setShowOption(option => !option)
-  }
-
+  const [searchQuery, setSearchQuery] = useState('');
+  const tools = data.filter((tool) => tool.title.toLowerCase().match(searchQuery));
   return (
     <ScrollView style={{ backgroundColor: 'white' }}>
       <View style={styles.mainContainer}>
         <View style={styles.container}>
           <View style={styles.header}>
             <GoBack />
-
-            <TouchableOpacity onPress={handleToggle} style={styles.optionButton}>
-              <Entypo name="dots-three-vertical" size={15} color="#47474F" />
+          </View>
+          <View style={styles.searchBar}>
+            <TextInput
+              value={searchQuery}
+              style={{ flex: 1 }}
+              placeholder="Search for tools, categories"
+              onChangeText={(text) => setSearchQuery(text)}
+            />
+            <TouchableOpacity style={styles.sortButton}>
+              <MaterialIcons name="sort" />
             </TouchableOpacity>
           </View>
         </View>
-        {showOption && (
-          <View style={styles.selectCard}>
-            <TouchableOpacity>
-              <Text style={styles.selectText}>Edit Tool</Text>
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <Text style={styles.selectText}>Delete Tool</Text>
-            </TouchableOpacity>
-          </View>
-        )}
-        <Image
-          resizeMode='contain'
-          style={styles.toolImage}
-          source={require('../../../assets/images/tool.png')}
-        />
-        <View style={styles.borderLine} />
-        <View style={styles.bodyContainer}>
-          <View style={styles.textCon}>
-            <Text style={styles.title}>Electric drilling</Text>
-            <Text style={styles.category}>Machine</Text>
-          </View>
-          <View style={styles.detailCon}>
-            <View style={styles.subDetailCon}>
-              <Text style={styles.category}>Current Status</Text>
-              <View style={styles.successTagCon}>
-                <View style={styles.successDot} />
-                <Text style={{ fontSize: 8.72, fontWeight: '400' }}>Available</Text>
-              </View>
-            </View>
-            <View style={styles.subDetailCon}>
-              <Text style={styles.category}>No of tools available</Text>
-              <View style={styles.errorTagCon}>
-                <Text style={{ fontSize: 8.72, fontWeight: '400' }}>17</Text>
-              </View>
-            </View>
-          </View>
-
-          <Text style={styles.description}>
-            This tool is intended for light-duty applications where it must drill tiny holes quickly.
-            Drilling small holes requires a high speed and hand feed. Bolts and nuts are used to
-            mount the machine's base on the floor or on a bench.
-          </Text>
-        </View>
+        {tools.map((tool, index) => {
+          return (
+            <Tool
+              title={tool.title}
+              category={tool.category}
+              description={tool.description}
+              status={tool.status as TOOL_STATUS}
+              lastUsed={tool.lastUsed}
+              id={tool.id}
+              key={index}
+            />
+          );
+        })}
       </View>
     </ScrollView>
   );
@@ -81,7 +108,6 @@ const MyToolsScreen = () => {
 const stylesheet = createStyleSheet((theme) => ({}));
 
 export default MyToolsScreen;
-
 
 const _styles = createStyleSheet((theme) => ({
   mainContainer: {
@@ -96,12 +122,28 @@ const _styles = createStyleSheet((theme) => ({
   bodyContainer: {
     padding: theme.margins.containerMargin,
   },
+  searchBar: {
+    flexDirection: 'row',
+    flex: 1,
+    gap: 10,
+    alignItems: 'center',
+  },
   header: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     width: '100%',
     alignItems: 'center',
-    marginTop: 20
+    marginVertical: 20,
+  },
+  sortButton: {
+    borderColor: theme.colors.gray,
+    borderWidth: 0.5,
+    borderRadius: 8,
+    padding: 10,
+    height: 45,
+    width: 45,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   subDetailCon: {
     flexDirection: 'row',
@@ -120,13 +162,13 @@ const _styles = createStyleSheet((theme) => ({
     top: 105,
     right: theme.margins.containerMargin,
     zIndex: 5,
-    backgroundColor: theme.colors.white
+    backgroundColor: theme.colors.white,
   },
   selectText: {
     fontSize: 14,
     fontWeight: '400',
     color: theme.colors.text3,
-    height: 25
+    height: 25,
   },
   optionButton: {
     padding: theme.margins.xl,
@@ -134,13 +176,13 @@ const _styles = createStyleSheet((theme) => ({
     alignItems: 'center',
     borderColor: theme.colors.gray,
     borderWidth: 0.5,
-    borderRadius: 8
+    borderRadius: 8,
   },
   successDot: {
     width: 4.98,
     height: 4.98,
     backgroundColor: theme.colors.success,
-    borderRadius: 3
+    borderRadius: 3,
   },
   successTagCon: {
     flexDirection: 'row',
@@ -150,7 +192,7 @@ const _styles = createStyleSheet((theme) => ({
     width: 64,
     height: 20,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   errorTagCon: {
     flexDirection: 'row',
@@ -160,20 +202,20 @@ const _styles = createStyleSheet((theme) => ({
     width: 27.92,
     height: 20,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   description: {
     fontSize: 14,
     fontWeight: '300',
     marginTop: 12,
     color: theme.colors.text2,
-    lineHeight: 19.6
+    lineHeight: 19.6,
   },
   toolImage: {
     width: '100%',
     height: 400,
     marginTop: -20,
-    zIndex: 2
+    zIndex: 2,
   },
   borderLine: {
     width: '100%',
@@ -183,18 +225,18 @@ const _styles = createStyleSheet((theme) => ({
   title: {
     fontSize: 16,
     fontWeight: '500',
-    color: theme.colors.text
+    color: theme.colors.text,
   },
   textCon: {
     alignItems: 'center',
     gap: 3,
-    flexDirection: 'row'
+    flexDirection: 'row',
   },
   detailCon: {
     alignItems: 'center',
     flexDirection: 'row',
     marginTop: 10,
-    gap: 10
+    gap: 10,
   },
   category: {
     fontSize: 12,
---
+++ components/Tool.tsx
@@ -0,0 +1,116 @@
+import { View, Text, Image } from 'react-native';
+import React from 'react';
+import { createStyleSheet, useStyles } from 'react-native-unistyles';
+
+export type TOOL_STATUS = 'available' | 'unavailable';
+export interface ToolProps {
+  id: string | number;
+  title: string;
+  category: string;
+  description: string;
+  status: TOOL_STATUS;
+  lastUsed: Date | string | number;
+}
+
+const Tool = ({ category, description, lastUsed, status, title }: ToolProps) => {
+  const { styles } = useStyles(_style);
+  const isAvailable = status == 'available';
+  return (
+    <View style={styles.toolWrapper}>
+      <Image
+        resizeMode="contain"
+        style={styles.toolImage}
+        source={require('../assets/images/tool.png')}
+      />
+      <View style={styles.toolContent}>
+        <View style={styles.titleWrapper}>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.category}>{category}</Text>
+        </View>
+        <Text style={styles.description}>{description.slice(0, 30)}</Text>
+        <Text>
+          <Text style={styles.timeUsed}>Last Used:</Text>
+          <Text style={styles.category}>{new Date(lastUsed).toLocaleString()}</Text>
+        </Text>
+      </View>
+
+      <View
+        style={[styles.status, isAvailable ? styles.availableStatus : styles.unavailableStatus]}>
+        <View style={[isAvailable ? styles.availableIndicator : styles.unavailableIndicator]} />
+        <Text style={styles.statusText}>{status}</Text>
+      </View>
+    </View>
+  );
+};
+
+const _style = createStyleSheet((theme) => ({
+  toolWrapper: {
+    flexDirection: 'row',
+    padding: 20,
+    alignItems: 'flex-start',
+  },
+  toolContent: {
+    flex: 1,
+  },
+  titleWrapper: {
+    flexDirection: 'row',
+    alignItems: 'baseline',
+    gap: 15,
+  },
+  toolImage: {
+    width: 50,
+    height: 50,
+    zIndex: 2,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '500',
+    color: theme.colors.text,
+  },
+  category: {
+    fontSize: 12,
+    fontWeight: '400',
+    color: theme.colors.text2,
+  },
+  timeUsed: {
+    fontWeight: '700',
+  },
+  description: {
+    fontSize: 13,
+    marginVertical: 5,
+    fontWeight: '400',
+    color: theme.colors.text2,
+    lineHeight: 19.5,
+  },
+  status: {
+    padding: 12,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 4,
+    borderRadius: 20,
+  },
+  statusText: {
+    fontSize: 12,
+    textTransform: 'capitalize',
+  },
+  availableIndicator: {
+    width: 10,
+    height: 10,
+    borderRadius: 10,
+    backgroundColor: theme.colors.success,
+  },
+  availableStatus: {
+    backgroundColor: theme.colors.successTag,
+  },
+  unavailableIndicator: {
+    width: 10,
+    height: 10,
+    borderRadius: 10,
+    backgroundColor: 'red',
+  },
+  unavailableStatus: {
+    backgroundColor: theme.colors.errorTag,
+  },
+}));
+
+export default Tool;
