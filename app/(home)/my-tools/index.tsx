8e1b414fb378ed867d60fad4452bbf2090c4ce8c
Original Timi <timileyinoyelekan11@gmail.com>
2024-07-30 08:49:52 +0100

    design: added default header

--- app/(home)/my-tools/index.tsx
+++ app/(home)/my-tools/index.tsx
@@ -1,10 +1,16 @@
+import { createStyleSheet } from 'react-native-unistyles';
+import HomeHeader from '~/components/modules/layout/home-header';
+import SearchBar from '~/components/SearchBar';
 import { Text, View } from '~/components/shared';
 
 const MyToolsScreen = () => {
   return (
     <View>
-      <Text>MyTools</Text>
+      <HomeHeader />
     </View>
   );
 };
+
+const stylesheet = createStyleSheet((theme) => ({}));
+
 export default MyToolsScreen;
